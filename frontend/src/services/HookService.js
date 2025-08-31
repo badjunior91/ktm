773a1726c62af74b1c59c5f09f80dfc15aa5cc4c
badjunior91 <badjunior91@gmail.com>
2025-08-31 13:42:19 +0300

    Ενημέρωση HookService.js

--- frontend/src/services/HookService.js
+++ frontend/src/services/HookService.js
@@ -2,19 +2,41 @@
 import authManager from "../managers/AuthManager.js";
 
 class HookService {
+    constructor() {
+        this.boundEvents = {};
+        console.log(this.boundEvents);
+
+    }
+
+    bindOnce(key, callback) {
+        if (this.boundEvents[key]) return;
+        this.boundEvents[key] = true;
+        console.log(this.boundEvents);
+        callback();
+    }
+
     useLogin() {
-        document.body.addEventListener("click", e => {
-            if (e.target.classList.contains("login_btn")) {
-                const username = document.querySelector("[data-name='username']").value;
-                const password = document.querySelector("[data-name='password']").value;
-                authManager.login(username, password);
-            }
+        this.bindOnce("login", () => {
+            document.body.addEventListener("click", e => {
+                if (e.target.classList.contains("login_btn")) {
+                    const username = document.querySelector("[data-name='username']").value;
+                    const password = document.querySelector("[data-name='password']").value;
+                    authManager.login(username, password);
+                }
+            });
         });
     }
 
     useDashboard() {
-        // bind κουμπιά dashboard
+        this.bindOnce("dashboard", () => {
+            document.body.addEventListener("click", e => {
+                if (e.target.classList.contains("db_user_logout")) {
+                    authManager.logout();
+                }
+            });
+        });
     }
 }
+
 
 export default new HookService();
